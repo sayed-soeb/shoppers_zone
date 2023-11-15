139b04fd8fcef99cbaa7f4a5eaa21328379d2527
sayed-soeb <sayed.soeb1@gmail.com>
2023-11-15 19:32:08 +0530

    module

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,15 @@
-// server.js
-
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import bcrypt from 'bcryptjs';
 import session from 'express-session';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
+app.use(cors());
 const PORT = 3001;
-
-import dotenv from 'dotenv';
 dotenv.config();
-
 // Connect to MongoDB
 mongoose.connect(process.env.DbUrl, {
   useNewUrlParser: true,
@@ -29,12 +26,10 @@ const userSchema = new mongoose.Schema({
 // Create a User model
 const User = mongoose.model('User', userSchema);
 
-app.use(cors());
 app.use(bodyParser.json());
-
 app.use(
   session({
-    secret: secret,
+    secret: 'yourSecretKey',
     resave: true,
     saveUninitialized: true,
   })
@@ -45,7 +40,26 @@ app.post('/signup', async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-    // ... (unchanged)
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: 'Signup successful' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -57,7 +71,26 @@ app.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // ... (unchanged)
+    // Check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    // Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    // Save the user data in the session
+    req.session.user = {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    res.json({ message: 'Login successful', user: req.session.user });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
